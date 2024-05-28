0d136efbff30873558131ca82e177c100dc8c7ef
Rustam <nuriev.rustam2015@yandex.ru>
2024-05-28 20:03:18 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+        String idea = "I like Java!" + " " + "But I am a newbie. ";
+
+        int year = 2024;
+
+        System.out.println(idea + year);
+    }
+}
+
+// 2. Внутри метода main выполните следующие действия (каждое действие должно быть на отдельной строке, итого в методе получится 6 строк):
+//
+//         - создайте переменную с именем idea типа String и присвойте ей значение "I like Java!";
+//
+//         - выведите на консоль эту переменную через команду System.out.println(idea);
+//
+//         - присоедините к переменной idea строку "But I am a newbie.";
+//
+//         - создайте переменную year с типом int и присвойте ей текущий год.
+//
+//         - присоедините к переменной idea переменную year;
+//
+//         - выведите на консоль эту переменную через команду System.out.println(idea);
