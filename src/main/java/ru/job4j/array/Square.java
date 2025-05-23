c14e352592167e8fa5f1cb295f99b4151084176c
fghgrtugd <fghgrtugd@gmail.com>
2025-05-23 11:33:08 +0300

    Массивы в тестах [#505226]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -1,12 +1,13 @@
 package ru.job4j.array;
 
 public class Square {
-    public static void calculate(int bound) {
+    public static int[] calculate(int bound) {
         int[] result = new int[bound];
         for (int i = 0; i < result.length; i++) {
             result[i] = i * i;
             System.out.println("Array " + i + " = " + result[i]);
         }
+        return result;
     }
 
     public static void main(String[] args) {
