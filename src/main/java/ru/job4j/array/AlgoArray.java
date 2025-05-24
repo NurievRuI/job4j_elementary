ac6fa7d8769e47d8f5f43807cdcfce85facc1be1
fghgrtugd <fghgrtugd@gmail.com>
2025-05-24 08:58:34 +0300

    Упорядочить массив [#505228 #544344]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,21 +1,23 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
+        int[] array = new int[]{5, 3, 2, 1, 4};
+
         int temp = array[0];
         array[0] = array[3];
         array[3] = temp;
 
-        for (int index = 0; index < array.length - 1; index++) {
-            if (array[index] > array[index + 1]) {
-                temp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = temp;
-            }
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+
+        for (int j : array) {
+            System.out.print(j + " ");
         }
-        System.out.println(Arrays.toString(array));
     }
 }
