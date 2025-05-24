3e50cdfd15cba620ec021e27194cf627481f1293
fghgrtugd <fghgrtugd@gmail.com>
2025-05-24 09:46:10 +0300

    Поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -12,6 +12,17 @@ public class FindLoop {
         return result;
     }
 
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == element) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         System.out.println(indexOf(data, 2));
