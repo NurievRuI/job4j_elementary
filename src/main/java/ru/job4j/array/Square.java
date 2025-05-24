23f6edd63a6a9c5927a43350110430a015944eb2
fghgrtugd <fghgrtugd@gmail.com>
2025-05-24 08:50:53 +0300

    Заполнить массив степенями чисел [#505225 #544140]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -5,7 +5,6 @@ public class Square {
         int[] result = new int[bound];
         for (int i = 0; i < result.length; i++) {
             result[i] = i * i;
-            System.out.println("Array " + i + " = " + result[i]);
         }
         return result;
     }
